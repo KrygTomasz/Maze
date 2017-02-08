fffbe5d4b2879c40ab7cf812c7abf9a5c32a7e00
KrygTomasz <yaami93@gmail.com>
2017-02-08 22:41:43 +0100

    Accelerometer and gravity implementation

--- Maze/GameVC.swift
+++ Maze/GameVC.swift
@@ -7,17 +7,26 @@
 //
 
 import UIKit
+import CoreMotion
 
 class GameVC: UIViewController {
 
     @IBOutlet weak var mainView: UIView!
-    var ball: Ball?
+    var ball: Ball!
+    
+    var motionManager = CMMotionManager()
+    var animator: UIDynamicAnimator?
+    var gravity: UIGravityBehavior?
+    var direction = CGVector(dx: 0.0, dy: 0.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initBall()
         mainView.layoutIfNeeded()
-        // Do any additional setup after loading the view.
+
+        createGravity()
+        
+        startAccelerometer()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,14 +34,50 @@ class GameVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     func initBall() {
         
-        guard let ball: Ball = UINib(nibName: "Ball", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? Ball else { return }
+        guard let ballObject: Ball = UINib(nibName: "Ball", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? Ball else { return }
+        self.ball = ballObject
         ball.setView()
         mainView.addSubview(ball)
         
-        ball.topAnchor.constraint(equalTo: mainView.topAnchor).isActive = true
-        ball.leftAnchor.constraint(equalTo: mainView.leftAnchor).isActive = true
+        //ball.topAnchor.constraint(equalTo: mainView.topAnchor).isActive = true
+        //ball.leftAnchor.constraint(equalTo: mainView.leftAnchor).isActive = true
+        
+    }
+    
+    func createGravity() {
+        
+        animator = UIDynamicAnimator(referenceView: mainView)
+        
+        gravity = UIGravityBehavior(items: [ball])
+        gravity?.gravityDirection = direction
+        
+        animator?.addBehavior(gravity!)
+        
+    }
+    
+    func startAccelerometer() {
+        
+        motionManager.accelerometerUpdateInterval = 0.1
+        motionManager.startAccelerometerUpdates(to: OperationQueue.main) {
+            data, _ in
+            if let accelerometerData = data {
+                self.direction.dx = CGFloat(3*accelerometerData.acceleration.x)
+                self.direction.dy = CGFloat(-3*accelerometerData.acceleration.y)
+                self.gravity?.gravityDirection = self.direction
+                
+                print("X: \(accelerometerData.acceleration.x)")
+                print("Y: \(accelerometerData.acceleration.y)")
+                print("Z: \(accelerometerData.acceleration.z)")
+            }
+            
+            // Do any additional setup after loading the view.
+        }
         
     }
     
--- Maze/ViewController.swift
+++ Maze/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setButtons()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -21,6 +22,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 
     func setButtons() {
